8694733db12c582b323bee6ca4a4645f50063373
Rakhi Jain <rakhujn@gmail.com>
2022-06-06 17:25:58 +0530

    the

---
+++ content_filtering.py
@@ -0,0 +1,37 @@
+# read csv file
+# remove all the NA Values
+# remove stop words
+# fite_transform
+# cosine_similarity method
+# replace the index number with title of the movie
+# based on that index number recommendations will be there
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('final.csv')
+df = df[df['soup'].notna()]
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(df['soup'])
+
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+df = df.reset_index()
+indices = pd.Series(df.index, index=df['title'])
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    return df[['title', 'poster_link', 'release_date', 'runtime', 'vote_average', 'overview']].iloc[movie_indices].values.tolist()
+
+
+
+# RRR movie=alia bhatt producer=ss rajamouli
+# ss rajamouli=bahubali
+
